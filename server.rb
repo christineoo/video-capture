90dd99c0b8fb461ffc8f84fe62600fb47bf07ed4
Christine Oo <christine@favoritemedium.com>
2015-04-07 15:34:55 +0900

    debug: change ffmpeg to output file to tmp folder

--- server.rb
+++ server.rb
@@ -70,11 +70,11 @@ post "/upload" do
       f.write(params['audio'][:tempfile].read)
     end
 
-    puts `ls public/videos`
+    puts `ls tmp`
     `ffmpeg -i uploads/#{uuid}.#{video_type} uploads/#{uuid}.mp4`
-    `ffmpeg -i uploads/#{uuid}.mp4 -i uploads/"#{uuid}_1".#{audio_type} -c:v copy-c:a aac -strict -2 experimental public/videos/#{uuid}.mp4`
+    `ffmpeg -i uploads/#{uuid}.mp4 -i uploads/"#{uuid}_1".#{audio_type} -c:v copy-c:a aac -strict -2 experimental #{directory}/tmp/#{uuid}.mp4`
 
-    f = File.new("#{directory}/#{uuid}.mp4", "r")
+    f = File.new("#{directory}/tmp/#{uuid}.mp4", "r")
     resp_mp4 = client.put_file("#{uuid}.mp4", f)
 
   end
